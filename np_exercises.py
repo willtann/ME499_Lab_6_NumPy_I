9a8d9727521cec2950cdc190aa19eb1b7f0160a1
Tanner WIlliams <97181243+willtann@users.noreply.github.com>
2022-02-19 22:54:50 -0800

    Saturday 02.20.2022

---
+++ np_exercises.py
@@ -0,0 +1,3 @@
+#! Users/tannerwilliams/Desktop/ME499/ME499_Lab6_NumPy_I/np_exercises.py
+import numpy as np
+
---
+++ testing.py
@@ -0,0 +1,4 @@
+#! Users/tannerwilliams/Desktop/ME499/ME499_Lab6_NumPy_I/testing.py
+import numpy as np
+
+
