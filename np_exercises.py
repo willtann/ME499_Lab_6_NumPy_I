4d3f8d82c869248649270ab731eb1345f032bc41
Tanner WIlliams <97181243+willtann@users.noreply.github.com>
2022-02-21 16:58:04 -0800

    Monday 02.22.2022

--- np_exercises.py
+++ np_exercises.py
@@ -65,20 +65,59 @@ def simulate_dice_rolls(num_rolls, iterations):
     return scores
 
 
+def is_transformation_matrix(trans_matrix):
+    """
+    :param trans_matrix:
+    :return:
+    """
+    rot_matrix = np.delete(trans_matrix, 3, 0)  # Get rid of the last row
+    rot_matrix = np.delete(rot_matrix, 3, 1)  # Get rid of the last column
+
+    # Now we have the rotation matrix (3x3)
+    trans = rot_matrix.transpose()  # Calculate the transpose of the rotation matrix
+    inv = np.linalg.inv(rot_matrix)  # Calculate tbe inverse of the rotation matrix
+
+    # If they are identical then the rotation matrix is valid
+    valid = np.array_equal(trans, inv)  # [1]
+    if valid is True:
+        return True
+    else:
+        return False
+
+
+def nearest_neighbors(points, target, cutoff_dist):
+    dist = np.array(np.linalg.norm(points-target, ord=2, axis=1))
+    print(dist)
+    dist = dist[dist < cutoff_dist]
+    # dist = np.reshape(dist, (points.shape[0], 1))
+    indexes = np.argsort(dist)
+    return points[indexes]
+
+
+
 if __name__ == '__main__':
-    a = np.arange(15).reshape(3, 5)
-    b = np.arange(15).reshape(3, 5)
+    # a = np.arange(15).reshape(3, 5)
+    # b = np.arange(15).reshape(3, 5)
+    #
+    # print('numpy_close: ', numpy_close(a, b))
+    # my_func = lambda x: x**2
+    # print('simple_minimizer: ', simple_minimizer(my_func, -1.75, 2.25, num=5))
 
-    print('numpy_close: ', numpy_close(a, b))
-    my_func = lambda x: x**2
-    print('simple_minimizer: ', simple_minimizer(my_func, -1.75, 2.25, num=5))
-
-    num_rolls = 5
-    iterations = 2000
+    # num_rolls = 5
+    # iterations = 2000
     # print(np.zeros(num_rolls))
     # print(r.randint(0, num_rolls))
     # a = np.random.randint(0, num_rolls, num_rolls)  # [3]
     # print(np.sum(a))
 
     # print(np.array(np.sum(a), iterations))
-    simulate_dice_rolls(num_rolls, iterations)
+    # simulate_dice_rolls(num_rolls, iterations)
+
+    # tf_valid = np.array([[0, 0, -1, 4], [0, 1, 0, 2.4], [1, 0, 0, 3], [0, 0, 0, 1]])
+    # tf_invalid = np.array([[1, 2, 3, 1], [0, 1, -3, 4], [0, 1, 1, 1], [-0.5, 4, 0, 2]])
+    # print(is_transformation_matrix(tf_valid))  # True
+    # print(is_transformation_matrix(tf_invalid))  # False
+
+    array = np.array([[1, 1, 1], [2, 3, 5], [0, 1, 1], [1.5, 1, 1], [10, 9, 9]])
+    target_pt = np.array([0, 1, 1])
+    print(nearest_neighbors(array, target_pt, 3.0))
